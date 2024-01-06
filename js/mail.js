040e3703b144865c1d53bc78c675c397e942b003
Michał <m.majewski.dm@gmail.com>
2024-01-07 00:26:06 +0100

    contact form update

--- js/mail.js
+++ js/mail.js
@@ -1,23 +1,23 @@
-const msgStatus = document.querySelector('.contact__form-msg-status')
+// const msgStatus = document.querySelector('.contact__form-msg-status')
 
-console.log(document.location.search)
+// console.log(document.location.search)
 
-const handleMailForm = () => {
+// const handleMailForm = () => {
 
-if (document.location.search === '?mail_status=sent') {
-	msgStatus.classList.add('success')
-	msgStatus.textContent = 'Wiadomość wysłana!'
+// if (document.location.search === '?mail_status=sent') {
+// 	msgStatus.classList.add('success')
+// 	msgStatus.textContent = 'Wiadomość wysłana!'
 
-	setTimeout(() => {
-		msgStatus.classList.remove('success')
-	}, 3000)
-}
+// 	setTimeout(() => {
+// 		msgStatus.classList.remove('success')
+// 	}, 3000)
+// }
 
-if (document.location.search === '?mail_status=error') {
-	msgStatus.classList.add('error')
-	msgStatus.textContent = 'Błąd wysyłania!!'
+// if (document.location.search === '?mail_status=error') {
+// 	msgStatus.classList.add('error')
+// 	msgStatus.textContent = 'Błąd wysyłania!!'
 
-	setTimeout(() => {
-		msgStatus.classList.remove('error')
-	}, 3000)
-}}
+// 	setTimeout(() => {
+// 		msgStatus.classList.remove('error')
+// 	}, 3000)
+// }}
