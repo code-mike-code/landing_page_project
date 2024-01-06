01befc3ce4feb6daa829c3dd0925aa2c1557c734
Michał <m.majewski.dm@gmail.com>
2024-01-06 23:54:56 +0100

    footer update, contact form update, mail.php file added, mail.js file added, some media queries updates

---
+++ js/mail.js
@@ -0,0 +1,21 @@
+const msgStatus = document.querySelector('.contact__form-msg-status')
+
+console.log(document.location.search)
+
+if (document.location.search === '?mail_status=sent') {
+	msgStatus.classList.add('success')
+	msgStatus.textContent = 'Wiadomość wysłana!'
+
+	setTimeout(() => {
+		msgStatus.classList.remove('success')
+	}, 3000)
+}
+
+if (document.location.search === '?mail_status=error') {
+	msgStatus.classList.add('error')
+	msgStatus.textContent = 'Błąd wysyłania!!'
+
+	setTimeout(() => {
+		msgStatus.classList.remove('error')
+	}, 3000)
+}
