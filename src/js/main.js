b76cb490d08a1692504b79676972f3d58a1cbc73
Michał <m.majewski.dm@gmail.com>
2024-06-19 20:52:52 +0200

    use gulp boundler, added a desktop menu, create components for nav bar menu, contact form and footer

---
+++ src/js/main.js
@@ -0,0 +1,86 @@
+const nav = document.querySelector('.nav')
+const navBtn = document.querySelector('.burger-btn')
+const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
+const msgStatus = document.querySelector('.contact__form-msg-status')
+
+const navDesktop = document.querySelector('.desktop-nav')
+
+
+
+const handleNav = () => {
+	nav.classList.toggle('nav--active')
+
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
+	handleNavItemsAnimation()
+}
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
+
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-item-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
+}
+
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+const scrollFunction = () => {
+
+	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+		navDesktop.classList.add("desktop-nav--transparent");
+	} else {
+		navDesktop.classList.remove("desktop-nav--transparent");
+	}
+}
+
+
+
+
+const handleMailForm = () => {
+	if (document.location.search === '?mail_status=sent') {
+		msgStatus.classList.add('success')
+		msgStatus.textContent = 'Wiadomość wysłana!'
+
+		setTimeout(() => {
+			msgStatus.classList.remove('success')
+		}, 3000)
+	}
+
+	if (document.location.search === '?mail_status=error') {
+		msgStatus.classList.add('error')
+		msgStatus.textContent = 'Błąd wysyłania!!'
+
+		setTimeout(() => {
+			msgStatus.classList.remove('error')
+		}, 3000)
+	}
+}
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+handleMailForm()
+handleCurrentYear()
+navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
+window.onscroll = () => scrollFunction()
